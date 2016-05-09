f3983c7b382725d110349e66293f73c24ba4cccb
Konstantin Bulenkov <kb@jetbrains.com>
2016-05-09 21:37:16 +0200

    scripts for project initialisation

---
+++ getModules.bat
@@ -0,0 +1,7 @@
+call git clone git@github.com:JetBrains/jdk8u_corba.git corba
+call git clone git@github.com:JetBrains/jdk8u_hotspot.git hotspot
+call git clone git@github.com:JetBrains/jdk8u_jaxp.git jaxp
+call git clone git@github.com:JetBrains/jdk8u_jaxws.git jaxws
+call git clone git@github.com:JetBrains/jdk8u_jdk.git jdk
+call git clone git@github.com:JetBrains/jdk8u_langtools.git langtools
+call git clone git@github.com:JetBrains/jdk8u_nashorn.git nashorn
---
+++ getModules.sh
@@ -0,0 +1,7 @@
+git clone git@github.com:JetBrains/jdk8u_corba.git corba
+git clone git@github.com:JetBrains/jdk8u_hotspot.git hotspot
+git clone git@github.com:JetBrains/jdk8u_jaxp.git jaxp
+git clone git@github.com:JetBrains/jdk8u_jaxws.git jaxws
+git clone git@github.com:JetBrains/jdk8u_jdk.git jdk
+git clone git@github.com:JetBrains/jdk8u_langtools.git langtools
+git clone git@github.com:JetBrains/jdk8u_nashorn.git nashorn
